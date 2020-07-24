6fcf06b0eae1f81c1e206e37068748ca0ca4ed3b
hfyy456 <540363975@qq.com>
2020-07-25 00:26:05 +0800

    更新时差问题

--- clock.py
+++ clock.py
@@ -1,9 +1,19 @@
+import datetime
+import time
 import requests
 from urllib.parse import urlencode
+
+
 def clock_in(data):
     print(data)
     url = "https://v-xxtb.zust.edu.cn/api/Ncov2019/update_record_student"
-
+    t = datetime.datetime.now()
+    t1 = t.strftime('%Y-%m-%d %H:%M:%S')
+    ts1 = time.mktime(time.strptime(t1, '%Y-%m-%d %H:%M:%S'))
+    ts2 = 1595607693.0
+   # end_time = int(str(ts1 * 1000).split(".")[0])
+   # start_time = int(str(ts2 * 1000).split(".")[0])
+    days = int((datetime.datetime.fromtimestamp(ts1) - datetime.datetime.fromtimestamp(ts2)).days)
     headers = {
         'Referer': 'https://v-xxtb.zust.edu.cn/web/mobile37/',
         'Origin': 'https://v-xxtb.zust.edu.cn',
@@ -29,7 +39,7 @@ def clock_in(data):
         "location_city": data['city'],
         "location_country": data['country'],
         "student_id": data['student_id'],
-        "r_id": 170,
+        "r_id": 171 + days,
         'round': '',
         "module_id": 63,
     }
@@ -37,4 +47,3 @@ def clock_in(data):
     print(payload)
     response = requests.request("POST", url, headers=headers, data=payload)
     return response.json()
-
--- task.py
+++ task.py
@@ -1,17 +1,26 @@
 import datetime
+import time
+
 from User import User
 import clock
-user=User()
+
+user = User()
+
 
 def my_job():
-    H =datetime.datetime.now().strftime('%H')
-    if( H !='8'):
+    H = datetime.datetime.now().strftime('%H')
+    print(H)
+    if (H != '08'):
         return
     else:
         results = user.findAll()
         for item in results:
-            if(item['student_id']):
-                log=clock.clock_in(item)
+            if (item['student_id']):
+                log = clock.clock_in(item)
                 print(log)
+                time.sleep(1)
         return
 
+
+if __name__ == '__main__':
+    my_job()
